6504b6d0580489c4d36c621c197ad375cde3f75c
Jamaalul Lail <a.jamaalul@gmail.com>
2025-06-21 12:23:51 +0700

    Menambah animasi ke umkm.show

--- resources/views/umkm/show.blade.php
+++ resources/views/umkm/show.blade.php
@@ -7,24 +7,30 @@
     <title>Berita | Cepokolimo</title>
     <link rel="stylesheet" href="{{ asset('css/global.css') }}">
     <link rel="stylesheet" href="{{ asset('css/umkm/show.css') }}">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
 <body>
     @include('components.navbar')
     <section class="content">
-        <div class="nama">
+        <div class="nama" data-aos="zoom-out-down" data-aos-duration="1000">
             <h1>{{ $umkm['nama'] }}</h1>
         </div>
-        <p class="owner">{{ $umkm['owner'] }}</p>
-        <div class="desc">
+        <p class="owner" data-aos="zoom-out-down" data-aos-duration="1000">{{ $umkm['owner'] }}</p>
+        <div class="desc" data-aos="zoom-out" data-aos-duration="1000">
             <p>{!! $umkm['deskripsi'] !!}</p>
         </div>
-        <p class="contact"><img src="{{ asset('assets/contact.svg') }}" alt=""> {{ $umkm['kontak'] }}</p>
+        <p class="contact" data-aos="zoom-out-up" data-aos-duration="1000"><img src="{{ asset('assets/contact.svg') }}" alt=""> {{ $umkm['kontak'] }}</p>
     </section>
     <section class="images">
         @foreach ($umkm['gambar'] as $image)
-            <div class="gambar" style="background-image: url('{{ asset('storage/' . $image) }}')"></div>
+            <div class="gambar" style="background-image: url('{{ asset('storage/' . $image) }}')" data-aos="fade-up" data-aos-duration="1000"></div>
         @endforeach
     </section>
     @include('components.footer')
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script type="module">
+      AOS.init();
+    </script>
 </body>
 </html>
